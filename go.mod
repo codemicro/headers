093574d8805ccf664df890d926c7dd2a9afa70e1
AKP <tom@tdpain.net>
2021-06-09 16:47:18 +0100

    Mage: update build location

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/codemicro/headers
 go 1.16
 
 require (
+	github.com/codemicro/alib-go v0.1.0 // indirect
 	github.com/magefile/mage v1.11.0 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.0-beta.3 // indirect
 	github.com/urfave/cli/v2 v2.3.0 // indirect
--- magefile.go
+++ magefile.go
@@ -2,8 +2,23 @@
 
 package main
 
-import "github.com/magefile/mage/sh"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+
+	"github.com/codemicro/alib-go/mage/exmg"
+	"github.com/magefile/mage/sh"
+)
 
 func Build() error {
-	return sh.Run("go", "build", "github.com/codemicro/headers/cmd/headers")
+
+	const buildPackage = "github.com/codemicro/headers/cmd/headers"
+
+	outputDir := filepath.Join("bin", fmt.Sprintf("%s-%s", exmg.GetTargetOS(), exmg.GetTargetArch()))
+	basePackageName := filepath.Base(buildPackage)
+
+	_ = os.MkdirAll(outputDir, os.ModeDir)
+
+	return sh.Run("go", "build", "-o", filepath.Join(outputDir, basePackageName), buildPackage)
 }
